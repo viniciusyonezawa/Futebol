f3b3b4fbde7c35997a4862d119dcc0fe583f1ae7
vinicius <viyonezawa@hotmail.com>
2018-09-24 21:03:37 -0300

    Descrição do commit

---
+++ meusite/cadastro/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+class Time(models.Model) :
+    nomeTime = models.CharField(max_length=50)
+    estadio = models.CharField(max_length=50)
+    patrocinio = models.CharField(max_length=50)
+    nomeTecnico = models.CharField(max_length=50)
+    nomeJogador = models.CharField(max_length=50)
+
